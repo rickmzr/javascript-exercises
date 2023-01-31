66affd58aba016c9b1084061f2ea655553f26fbf
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-01-30 21:27:10 -0600

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    const newArray = [];
+    for (let arrIterable = 0; arrIterable < arr.length; arrIterable++) {
+        for (let paramIterable = 0; paramIterable < toRemove.length; paramIterable++) {
+            if (toRemove[paramIterable] === arr[arrIterable]) {
+                break;
+            }
+            if (paramIterable === (toRemove.length - 1)) {
+                newArray.push(arr[arrIterable]);
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
