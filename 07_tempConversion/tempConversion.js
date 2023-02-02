f604563b607ecd5b419a95c575337c1273c01e54
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-02-01 20:29:16 -0600

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  return +((fahrenheitTemp - 32) / (9/5)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return +(celsiusTemp * 9 / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
