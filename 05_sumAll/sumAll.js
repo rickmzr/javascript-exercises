b409891148d821b7b01961e01fdd65af3d626a58
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-02-01 19:56:58 -0600

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let smallerInt = 0;
+    let biggerInt = 0;
+    let sum = 0;
+    if (typeof(int1) !== "number" || typeof(int2) !== "number" ||
+        int1 < 0 || int2 < 0) {
+        return "ERROR";
+    }
+    if (int1 < int2) {
+        smallerInt = int1;
+        biggerInt = int2
+    } else {smallerInt = int2;
+            biggerInt = int1;}
+    for (let i = smallerInt; i <= biggerInt; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
