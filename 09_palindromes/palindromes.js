49df741addc9c94868b71f06aa5aefc6c0f715c5
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-02-07 20:49:29 -0600

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let strArr = string.toLowerCase().split("");
+    let filteredArr = strArr.filter(function(character) {
+        return /[a-z]/.test(character);
+    });
+    const reversedArr = [...filteredArr].reverse();
+    for (let i = 0; i < filteredArr.length; i++) {
+        if (filteredArr[i] !== reversedArr[i]) {
+            return false;
+        };
+    };
+    return true;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
