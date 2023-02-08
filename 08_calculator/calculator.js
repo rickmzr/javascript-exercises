d2490208cc2154b6d9ad4a5d5a829a49e17e7937
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-02-07 20:12:42 -0600

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(int1, int2) {
+	return int1 + int2;
 };
 
-const subtract = function() {
-	
+const subtract = function(int1, int2) {
+	return int1 - int2;
 };
 
-const sum = function() {
-	
+const sum = function(numberArr) {
+	return numberArr.reduce((acc, num) => 
+    acc + num
+  , 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  return numArr.reduce((acc, num) => 
+    acc * num
+  , 1)
 };
 
-const power = function() {
-	
+const power = function(int1, int2) {
+	return int1 ** int2;
 };
 
-const factorial = function() {
-	
+const factorial = function(int) {
+	if (int === 0) {
+    return 1;
+  };
+  let total = 1;
+  for (let i = int; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
