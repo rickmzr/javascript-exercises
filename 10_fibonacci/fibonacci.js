d65ef38edd7e44f3cb6113bd123bde8922363016
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-02-10 19:02:53 -0600

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    position = +position;
+    if (position <= 0) {
+        return "OOPS";
+    };
+    if (position === 1) {
+        return 1;
+    } else if (position === 2) {
+        return 1;
+    };
+    let fibArray = [1, 1];
+    for (let i = 2; i <= position; i++) {
+        fibArray.push(fibArray[i-2]+fibArray[i-1]);
+    }
+    return fibArray[position-1];
 };
 
 // Do not edit below this line
