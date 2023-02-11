b8f743c8f96db10db71562007a2e9e94cdf30c30
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-02-10 20:18:38 -0600

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((p,c,i,a) => {
+        if (!c.hasOwnProperty('yearOfDeath')) {
+            c.yearOfDeath = new Date().getFullYear();
+        };
+        if (i === 0) {
+            return c;
+        }
+        if ((c.yearOfDeath - c.yearOfBirth) > (p.yearOfDeath - p.yearOfBirth)) {
+            return c;
+        } else {return p};
+    }, {})
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
